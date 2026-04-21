6e64800be06782d2f39ab31e277462c34d0ded47
venen-jean <venenjean@proton.me>
2026-04-21 07:57:43 +0200

    Renamed button

--- Projekt/pages/RoleForm.Designer.cs
+++ Projekt/pages/RoleForm.Designer.cs
@@ -48,7 +48,7 @@
             this.public_usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.hrbac_rolesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.hrbac_users_rolesBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -100,7 +100,7 @@
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.button5);
             this.tabPage2.Controls.Add(this.getPubBtn);
             this.tabPage2.Controls.Add(this.game_platformsDataGridView);
             this.tabPage2.Controls.Add(this.public_usersDataGridView);
@@ -212,13 +212,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(445, 253);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Get Devs";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.GetDevBtn_Click);
+            this.button5.Location = new System.Drawing.Point(445, 253);
+            this.button5.Name = "button1";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Get Devs";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.GetDevBtn_Click);
             // 
             // RoleForm
             // 
@@ -263,6 +263,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
         private System.Windows.Forms.BindingSource game_platformsBindingSource;
         private System.Windows.Forms.Button getPubBtn;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
     }
 }
